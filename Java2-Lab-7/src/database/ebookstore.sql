e2bad5ebdd3cf5d2fe30b4cb4b5c5f32fdd60f26
hellvn <vuongpt93@gmail.com>
2020-11-14 02:03:36 +0700

    lab7

---
+++ Java2-Lab-7/src/database/ebookstore.sql
@@ -0,0 +1,208 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2020 at 08:02 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ebookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `bookID` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `author` varchar(30) NOT NULL,
+  `published` int(4) NOT NULL,
+  `qty` int(11) DEFAULT 0,
+  `price` float(10,0) DEFAULT 0,
+  `status` int(11) DEFAULT 1,
+  `created` date DEFAULT current_timestamp(),
+  `updated` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `no` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `phone` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`no`, `username`, `name`, `address`, `email`, `phone`) VALUES
+(1, 'clark', 'clark ken', 'america', 'kent@amt.com', '1234567890'),
+(4, 'nhat', 'opticalsun', 'ha noi', 'nhatvi512001', '0964231456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdetail`
+--
+
+CREATE TABLE `orderdetail` (
+  `orderID` int(11) DEFAULT NULL,
+  `bookID` int(11) DEFAULT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `price` decimal(10,1) DEFAULT NULL,
+  `createddate` date DEFAULT NULL,
+  `updateddate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderID` int(11) NOT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `discount` int(11) DEFAULT NULL,
+  `total` decimal(10,1) DEFAULT NULL,
+  `orderdate` date DEFAULT NULL,
+  `status` int(11) DEFAULT NULL CHECK (`status` regexp '[0-5]{1}'),
+  `createddate` date DEFAULT NULL,
+  `updateddate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `role` int(1) NOT NULL DEFAULT 1,
+  `registed` date DEFAULT current_timestamp(),
+  `updated` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `role`, `registed`, `updated`) VALUES
+('admin', 'admin', 4, '2020-11-12', '2020-11-12'),
+('clark', '12345', 1, '2020-11-12', '2020-11-12'),
+('nhat', 'nhat123', 1, '2020-11-13', '2020-11-13');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`bookID`) USING BTREE,
+  ADD UNIQUE KEY `bookID` (`bookID`,`title`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`no`,`username`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  ADD KEY `fk_detail` (`bookID`,`title`),
+  ADD KEY `fk_detail2` (`orderID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderID`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`,`password`),
+  ADD KEY `fk_user` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `bookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+
+--
+-- Constraints for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  ADD CONSTRAINT `fk_detail` FOREIGN KEY (`bookID`,`title`) REFERENCES `books` (`bookID`, `title`),
+  ADD CONSTRAINT `fk_detail2` FOREIGN KEY (`orderID`) REFERENCES `orders` (`orderID`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_customer` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
