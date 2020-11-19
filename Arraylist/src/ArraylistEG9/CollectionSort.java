49c39e420024f4c50a05da448625bf23cbfab682
Morphine <64981166+hellvn@users.noreply.github.com>
2020-11-20 01:45:39 +0700

    arr

---
+++ Arraylist/src/ArraylistEG9/CollectionSort.java
@@ -0,0 +1,23 @@
+package ArraylistEG9;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionSort {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(13);
+        numbers.add(7);
+        numbers.add(18);
+        numbers.add(5);
+        numbers.add(2);
+
+        System.out.println("Before: " + numbers);
+
+        Collections.sort(numbers);
+
+        System.out.println("After: " + numbers);
+    }
+}
